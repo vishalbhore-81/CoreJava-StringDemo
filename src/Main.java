f04f902fcaf0c83123279c58c84cbff39bc5461e
Vishal Bhore <mr.vishalbhore@gmail.com>
2024-10-25 23:22:31 +0530

    String Demo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = new String("Bitcode");
+        String s2 = "Pune";
+
+        String s3 = "India";
+        String s4 = "India";
+
+        System.out.println(s3 == s4);
+    }
+}
